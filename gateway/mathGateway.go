70b6c97932089a9aad6607ff12c2db95979e313a
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-09 03:41:16 +0700

    feat: add SinCos method on math gateway and its implementation

--- gateway/mathGateway.go
+++ gateway/mathGateway.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	mv1 "github.com/alibaihaqi/golang-grpc/proto/math/v1"
 	"io"
+	"math"
+	"time"
 )
 
 type MathGateway struct {
@@ -14,6 +16,42 @@ func (m *MathGateway) Add(_ context.Context, req *mv1.AddRequest) (*mv1.AddRespo
 	return &mv1.AddResponse{
 		Result: req.GetFirstNumber() + req.GetSecondNumber(),
 	}, nil
+}
+
+func (m *MathGateway) SinCos(req *mv1.SinCosRequest, stream mv1.MathService_SinCosServer) error {
+	commonDegreesList := []int{0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360}
+
+	for _, degrees := range commonDegreesList {
+		radians := float64(degrees) * (math.Pi / 180)
+
+		var value float64 = 0
+
+		if req.Method == mv1.SineCosineEnum_SINE {
+			value = math.Sin(radians)
+		} else if req.Method == mv1.SineCosineEnum_COSINE {
+			value = math.Cos(radians)
+		}
+
+		threshold := 1e-10
+		if value > 0 && value < threshold {
+			value = math.Floor(value)
+		} else if value < 0 && value > -threshold {
+			value = math.Ceil(value)
+		}
+
+		res := &mv1.SinCosResponse{
+			Degree: int32(degrees),
+			Value:  float32(value),
+		}
+
+		time.Sleep(time.Second)
+
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *MathGateway) TotalNumber(stream mv1.MathService_TotalNumberServer) error {
